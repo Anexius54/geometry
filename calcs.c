eae50adb98ab3fb19f4780127d6b625b2b4ccfb1
Evgeniy Sapronov <ek.sapronov@gmail.com>
2019-05-16 21:00:57 +0700

    Create empty calcs.c, calcs.h

---
+++ calcs.c
@@ -0,0 +1,17 @@
+#include "input-output.h"
+#include <stdio.h>
+
+const float pi = 3.14;
+
+float perimeter(float radius)
+{
+    float p;
+    p = 2 * pi * radius;
+    return p;
+}
+
+float square(float radius)
+{
+    float s;
+    s = pi * radius * radius return s;
+}
---
+++ calcs.h
@@ -0,0 +1,7 @@
+#ifndef CALCS_H
+#define CALCS_H
+
+float perimeter(float radius);
+float square(float radius);
+
+#endif
